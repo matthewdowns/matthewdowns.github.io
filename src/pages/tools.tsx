568ff7342ba6b34f3d3824b20cf116e833e93c2e
Matthew Downs <matthew.downsc@gmail.com>
2023-04-10 20:29:54 -0500

    Fix for both tools page using same type name for data as the component

--- src/pages/tools.tsx
+++ src/pages/tools.tsx
@@ -4,42 +4,44 @@ import React, {
   useState
 } from 'react'
 
-interface Tool {
+type ToolsInfo = Array<{
   slug: string
   title?: string
   description?: string
   keywords?: string[]
-}
+}>
+
+const toolsInfoEmpty: ToolsInfo = [
+  { slug: 'shapefile.js' }
+]
 
 function Tools() {
-  const [tools, setTools] = useState<Tool[]>([
-    { slug: 'shapefile.js' }
-  ])
+  const [toolsInfo, setToolsInfo] = useState<ToolsInfo>(toolsInfoEmpty)
 
   useEffect(() => {
-    Promise.all(tools.map(async tool => {
+    Promise.all(toolsInfoEmpty.map(async toolInfo => {
       try {
-        const response = await fetch(`/tools/${tool.slug}`, {
+        const response = await fetch(`/tools/${toolInfo.slug}`, {
           method: 'GET'
         })
         if (response.ok) {
           const result = await response.text()
-          tool.title = result.match(/<title>(.*?)<\/title>/)?.at(1)
-          tool.description = result.match(/<meta name="description" content="(.*?)"\s?\/>/)?.at(1)
-          tool.keywords = result.match(/<meta name="keywords" content="(.*?)"\s?\/>/)?.at(1)?.split(',')
+          toolInfo.title = result.match(/<title>(.*?)<\/title>/)?.at(1)
+          toolInfo.description = result.match(/<meta name="description" content="(.*?)"\s?\/>/)?.at(1)
+          toolInfo.keywords = result.match(/<meta name="keywords" content="(.*?)"\s?\/>/)?.at(1)?.split(',')
         }
       } catch { }
-      return tool
-    })).then(setTools)
+      return toolInfo
+    })).then(setToolsInfo)
   }, [])
 
   return (
     <div className="mx-auto max-w-7xl p-4 sm:p-6 lg:p-8">
       <div className="grid grid-cols-12 gap-4">
-        {tools.map(tool => (
-          <div key={tool.slug} className="col-span-12 lg:col-span-6 xl:col-span-4 2xl:col-span-3">
+        {toolsInfo.map(toolInfo => (
+          <div key={toolInfo.slug} className="col-span-12 lg:col-span-6 xl:col-span-4 2xl:col-span-3">
             <div className="bg-slate-100 dark:bg-slate-800 rounded-xl p-5">
-              {tool.title == null ? (
+              {toolInfo.title == null ? (
                 <div role="status" className="max-w-sm animate-pulse">
                   <div className="h-5 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-4"></div>
                   <div className="h-3 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[240px] mb-4"></div>
@@ -54,20 +56,20 @@ function Tools() {
                 </div>
               ) : (
                 <div className="prose dark:prose-invert">
-                  <Link className="no-underline decoration-1 hover:underline" href={`/tools/${tool.slug}`}>
+                  <Link className="no-underline decoration-1 hover:underline" href={`/tools/${toolInfo.slug}`}>
                     <h3 className="mt-0">
-                      {tool.title}
+                      {toolInfo.title}
                     </h3>
                   </Link>
                   <div className="flex flex-row flex-wrap gap-2">
-                    {tool.keywords?.map(keyword => (
+                    {toolInfo.keywords?.map(keyword => (
                       <small key={keyword} className="bg-slate-200 dark:bg-slate-500 dark:text-white px-2 rounded-full">
                         {keyword}
                       </small>
                     ))}
                   </div>
                   <p>
-                    {tool.description}
+                    {toolInfo.description}
                   </p>
                 </div>
               )}
